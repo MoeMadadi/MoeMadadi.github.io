ea8fc641ca462d65430744508f6d48b9c07d31ee
MoeMadadi <95984854+MoeMadadi@users.noreply.github.com>
2025-12-15 02:03:05 +0000

    Deploying to gh-pages from @ MoeMadadi/MoeMadadi.github.io@59da1275479b508428127c45c9fd4e69a3c138b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
